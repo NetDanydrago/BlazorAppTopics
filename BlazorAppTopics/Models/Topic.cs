36f5a26dc762cc15f01560f151a9e7ea56cf979d
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2020-08-31 18:41:24 -0500

    Arreglar Tema Recursivo de Busqueda

---
+++ BlazorAppTopics/Models/Topic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppTopics.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public bool IsHasChildren { get; set; }
+        public List<Topic> ChildrenList { get; set; }
+        public bool IsOpen { get; set; }
+    }
+}
